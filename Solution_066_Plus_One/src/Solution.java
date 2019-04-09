962ce620d961f20ad9ec84ee2ac428321f80d250
ykim306 <ykim306@gmail.com>
2019-04-09 18:14:56 -0400

    Added iterative solution

--- Solution_066_Plus_One/src/Solution.java
+++ Solution_066_Plus_One/src/Solution.java
@@ -43,8 +43,6 @@ import java.util.Scanner;
 				
 				totSampleSet = sc.nextInt();
 				
-				target = sc.nextInt();
-				
 				for (int i = 0; i < totSampleSet; i++) {
 					int arraySize = sc.nextInt();
 					int[] numArray = new int[arraySize];
@@ -62,9 +60,21 @@ import java.util.Scanner;
 		
 		static private int[] plusOne(int[] digits) {
 			
+			for (int i = digits.length - 1; i >=0; i--) {
+				if (digits[i] != 9) {
+					digits[i]++;
+					break;
+				} else {
+					digits[i] = 0;
+				}
+			}
+			if (digits[0] == 0) {
+				int[] res = new int[digits.length+1];
+				res[0] = 1;
+				return res;
+			}
+			return digits;
 			
-			
-			return new int[] {0};
 		}
 		
 	}
