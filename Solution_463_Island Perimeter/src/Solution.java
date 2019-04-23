41c58bc1ab760248d09be387b90a9ab111b42aec
ykim306 <ykim306@gmail.com>
2019-04-23 17:07:38 -0400

    Added DFS Recursive Solution

--- Solution_463_Island Perimeter/src/Solution.java
+++ Solution_463_Island Perimeter/src/Solution.java
@@ -71,7 +71,8 @@ import java.util.Scanner;
 						System.out.println(Arrays.toString(a));
 					}
 					
-					answer = islandPerimeter(numArray);
+					// answer = islandPerimeter(numArray);
+					answer = islandPerimeterDFS(numArray);
 					
 					System.out.println(answer);
 				}
@@ -91,6 +92,40 @@ import java.util.Scanner;
 					}
 				}
 			}
+			
+			return count;
+		}
+		
+		static private int islandPerimeterDFS(int[][] grid) {
+			if (grid == null) return 0;
+			for (int i = 0 ; i < grid.length ; i++){
+				for (int j = 0 ; j < grid[0].length ; j++){
+					if (grid[i][j] == 1) {
+						return getPerimeter(grid,i,j);
+					}
+				}
+			}
+			return 0;
+		}
+
+		static private int getPerimeter(int[][] grid, int i, int j){
+			if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+				return 1;
+			}
+			
+			if (grid[i][j] == 0) {
+				return 1;
+			}
+			
+			if (grid[i][j] == -1) return 0;
+			
+			int count = 0;
+			grid[i][j] = -1;
+			
+			count += getPerimeter(grid, i-1, j);
+			count += getPerimeter(grid, i, j-1);
+			count += getPerimeter(grid, i, j+1);
+			count += getPerimeter(grid, i+1, j);
 			
 			return count;
 		}
